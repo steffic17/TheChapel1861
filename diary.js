d975558d1b594ad95ba2160291ce7ffef819f133
steffic17 <35765450+steffic17@users.noreply.github.com>
2022-01-20 14:17:50 +0000

    New version of the website
    With gallery and diary availability correct as of Jan '22

---
+++ diary.js
@@ -0,0 +1,278 @@
+const getDate = () => {
+	const d = new Date();
+}
+
+//Diary information
+const getDiaryData = () => {
+	
+    var diaryWeekDate = [
+		"03 Dec 2021",
+"10 Dec 2021",
+"17 Dec 2021",
+"24 Dec 2021",
+"31 Dec 2021",
+"07 Jan 2022",
+"14 Jan 2022",
+"21 Jan 2022",
+"28 Jan 2022",
+"04 Feb 2022",
+"11 Feb 2022",
+"18 Feb 2022",
+"25 Feb 2022",
+"04 Mar 2022",
+"11 Mar 2022",
+"18 Mar 2022",
+"25 Mar 2022",
+"01 Apr 2022",
+"08 Apr 2022",
+"15 Apr 2022",
+"22 Apr 2022",
+"29 Apr 2022",
+"06 May 2022",
+"13 May 2022",
+"20 May 2022",
+"27 May 2022",
+"03 Jun 2022",
+"10 Jun 2022",
+"17 Jun 2022",
+"24 Jun 2022",
+"01 Jul 2022",
+"08 Jul 2022",
+"15 Jul 2022",
+"22 Jul 2022",
+"29 Jul 2022",
+"05 Aug 2022",
+"12 Aug 2022",
+"19 Aug 2022",
+"26 Aug 2022",
+"02 Sep 2022",
+"09 Sep 2022",
+"16 Sep 2022",
+"23 Sep 2022",
+"30 Sep 2022",
+"07 Oct 2022",
+"14 Oct 2022",
+"21 Oct 2022",
+"28 Oct 2022",
+"04 Nov 2022",
+"11 Nov 2022",
+"18 Nov 2022",
+"25 Nov 2022"
+	];
+	var diaryWeekAvailability = [
+		false,
+false,
+false,
+false,
+false,
+false,
+false,
+false,
+false,
+false,
+false,
+false,
+false,
+false,
+true,
+true,
+true,
+false,
+false,
+true,
+true,
+false,
+true,
+true,
+true,
+false,
+true,
+true,
+true,
+true,
+true,
+true,
+true,
+true,
+true,
+false,
+false,
+true,
+true,
+true,
+true,
+true,
+true,
+true,
+true,
+true,
+false,
+true,
+false,
+false,
+false,
+false
+	];
+
+
+	var diaryNightPrice = [
+		120,
+179,
+243,
+243,
+243,
+154,
+120,
+120,
+120,
+120,
+120,
+120,
+120,
+179,
+154,
+154,
+154,
+179,
+204,
+204,
+179,
+179,
+154,
+154,
+154,
+243,
+179,
+179,
+179,
+204,
+204,
+204,
+243,
+243,
+243,
+243,
+243,
+243,
+243,
+204,
+204,
+204,
+179,
+179,
+179,
+204,
+204,
+179,
+154,
+154,
+120,
+120
+	];
+
+	const weekday = new Array(14);
+	weekday[0] = "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wednesday";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
+	weekday[7] = "Sunday";
+	weekday[8] = "Monday";
+	weekday[9] = "Tuesday";
+	weekday[10] = "Wednesday";
+	weekday[11] = "Thursday";
+	weekday[12] = "Friday";
+	weekday[13] = "Saturday";
+	
+	const month = new Array(12);
+	month[0] = "January";
+	month[1] = "February";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "August";
+	month[8] = "September";
+	month[9] = "October";
+	month[10] = "November";
+	month[11] = "December";
+	
+	let dateToday = new Date();
+	let allDiary = [];  // Initialise the object array
+	//Loop through whole date period
+	for (let i = 0; i < diaryWeekDate.length; i++){
+		const d = new Date(diaryWeekDate[i]);
+		for (let j = 0; j < 7 ; j++){
+			var newDate = new Date(d);
+			newDate.setDate(d.getDate() + j);
+			singleDiaryEntry = {
+				date: newDate,
+				day_num: d.getDay()+j,
+				day: weekday[d.getDay()+j],
+				availability: diaryWeekAvailability[i],
+				night_price: diaryNightPrice[i]
+			}
+		allDiary.push(singleDiaryEntry);
+		}
+	}
+	
+	let diaryInMonths = [];  // Initialise the object array
+	let currentMonthData = [];  // Initialise the object array
+	let currentDay = [];  // Initialise the object array
+	let singleDayEntry = [];  // Initialise the object array
+	
+	const firstDiaryDate = new Date(allDiary[0].date);
+	
+	let dayOfMonth = firstDiaryDate.getDay()-2;
+	let currentMonth = firstDiaryDate.getMonth();
+	for (let n = 0;n < allDiary.length; n++){
+		const currentDiaryDate = new Date(allDiary[n].date);
+//		console.log(currentDiaryDate);
+		if (currentDiaryDate.getMonth() != currentMonth){
+//			console.log(currentMonth);
+			// Push month only if the last day's in the future!
+			if(currentMonthData[currentMonthData.length-1].fullDate>dateToday){
+				diaryInMonths.push(currentMonthData);
+			}
+			currentMonthData = [];  // Re-initialise the object array
+//			console.log(`pushing month ${month[currentMonth]}`);
+			if (currentMonth != 11){
+				currentMonth++;
+				dayOfMonth = 1;
+			}
+			else{
+				currentMonth = 0;
+				dayOfMonth = 1;
+			}
+		}
+		currentDay = allDiary[n];
+//		console.log(firstDiaryDate);
+//		console.log(currentDay);
+		singleDayEntry = {
+			fullDate: currentDay.date,
+			month: month[currentMonth],
+			year: currentDay.date.getFullYear(),
+			dayOfMonth: dayOfMonth,
+			dayOfWeek: currentDay.day,
+			dayOfWeek_num: currentDay.day_num,
+			availability: currentDay.availability,
+			night_price: currentDay.night_price
+		}
+		
+		currentMonthData.push(singleDayEntry);
+		
+		dayOfMonth++;
+		
+	}
+	// Push month only if the last day's in the future!
+	if(currentMonthData[currentMonthData.length-1].fullDate>dateToday){
+		diaryInMonths.push(currentMonthData);
+	}
+
+//	console.log(diaryInMonths);
+	
+	return diaryInMonths;
+}
--- index.js
+++ index.js
@@ -1,61 +1,227 @@
-/*var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+/**/
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const $slideshowContainer = $('.slideshow-container');
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-*/
+//var selectSlide = document.getElementsByClassName('dot');
 
-
+//Initialise slide variables
 var slideIndex = 0;
-showSlides(slideIndex);
+var slides = [];
 
-// Next/previous controls
+
+//Call the slide data
+const getSlides = async () => {
+	
+	allSlides = getAllSlideData();
+		
+	return allSlides;
+}
+
+
+//Slide viewing functions
+const executeSlideView = (slideIndex=0) => {
+  $slideshowContainer.css("visibility", "visible");
+  
+  getSlides().then(slides => slides);
+  
+  slideIndex = checkSlideIndex(slideIndex, slides);
+  
+  renderSingleSlide(slides, slideIndex);
+  document.getElementById("slide-contents").style.display = "block";
+  return false;
+}
+
+const renderSingleSlide = (slides, slideIndex = 0) => {
+  slideIndex = checkSlideIndex(slideIndex, slides);
+  
+  const currentSlide = slides[slideIndex];
+  let slideContent = createSingleSlideHTML(currentSlide);
+  
+  document.getElementById("slide-contents").innerHTML = slideContent;
+}
+
+//Slide viewing HTML helper
+const createSingleSlideHTML = (currentSlide) => {
+  return `<div class = "numbertext"> ${currentSlide.slide_number} of ${currentSlide.total_slides} </div>
+	<img src = "${currentSlide.slide_image}" style = "${currentSlide.style}">
+	<div class = "text">${currentSlide.caption}<\div>
+    </div>
+	`;
+}
+
+
+
+
+
+//Check the slide number is in the correct range
+function checkSlideIndex(slideIndex, slides){
+  slideIndex = Math.min(slideIndex, slides.length-1);
+  slideIndex = Math.max(slideIndex, 0);
+  return slideIndex;
+}
+
+// Next/previous slide controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  slideIndex = checkSlideIndex(slideIndex += n, slides);
+  showSlides(slideIndex);
+  return slideIndex;
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+// Current slide image controls
+function selectAnySlide(n) {
+  slideIndex = checkSlideIndex(slideIndex = n, slides);
+  showSlides(slideIndex);
+  return slideIndex;
 }
 
 
+//Show slides with dot function
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+  
+  var slideIndex = n;
+  
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+
+
+  dots[slideIndex].className += " active";
+  executeSlideView(slideIndex);
+}
+
+
+
+
+
+
+
+// Initialise month variables
+var monthIndex = 0;
+var diaryData = [];
+
+
+//Call the diary data
+const getDiary = async () => {
+	allDiary = getDiaryData();
+	
+	return allDiary;
+}
+
+
+/* //Call the slide data
+const getSlides = async () => {
+	
+	allSlides = getAllSlideData();
+		
+	return allSlides;
+}
+
+//Slide viewing functions
+const executeSlideView = (slideIndex=0) => {
+  $slideshowContainer.css("visibility", "visible");
+  
+  getSlides().then(slides => slides);
+  
+  slideIndex = checkSlideIndex(slideIndex, slides);
+  
+  renderSingleSlide(slides, slideIndex);
+  document.getElementById("slide-contents").style.display = "block";
+  return false;
+}
+ */
+
+const showMonth = (monthIndex = 0) => {
+//  console.log("getting diary data");
+  getDiary().then(diaryData => diaryData);
+  
+  renderSingleMonth(allDiary, monthIndex);
+  return false;
+ }
+
+const renderSingleMonth = (diaryData, monthIndex = 0) => {
+  
+//  console.log(diaryData);
+  const currentMonth = diaryData[monthIndex];
+//  console.log(currentMonth);
+  let monthHTMLContent = createSingleMonthHTML(currentMonth);
+  
+  document.getElementById("view-month").innerHTML = `${currentMonth[0].month}<br><span style="font-size:18px">${currentMonth[0].year}</span>`;
+  document.getElementById("month-contents").innerHTML = monthHTMLContent;
+}
+
+//Slide viewing HTML helper
+const createSingleMonthHTML = (currentMonth) => {
+//  console.log(currentMonth);
+  let htmlString = "";  // Initialise the html string
+  // Pre-fill blanks incase first let doesn't occur on first day of month
+  for (let n = 0;n < currentMonth[0].dayOfMonth-1; n++){
+	  htmlString += `<li></li>`;
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {return}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+
+  // Pre-fill days before first day in month
+  for (let n = 0;n < currentMonth[0].dayOfWeek_num-currentMonth[0].dayOfMonth; n++){
+	  htmlString += `<li></li>`;
+  }
+  
+  dateToday = new Date();
+  //dateToday = new Date('April 19, 2022');  //Force date to check!
+  
+  for (let n = 0;n < currentMonth.length; n++){
+	if(currentMonth[n].fullDate>dateToday){
+		if (currentMonth[n].availability){
+			if (currentMonth[n].dayOfWeek == "Friday"){
+				htmlString += `<li><span class="start">
+				  <a href="mailto: thechapel1861@outlook.com?subject=Chapel 1861 Enquiry:  Availability ${currentMonth[n].dayOfMonth} ${currentMonth[n].month} ${currentMonth[n].year}"> ${currentMonth[n].dayOfMonth}
+				  <span style="font-size:0.7rem">£${currentMonth[n].night_price}</span></a></span></li>`;
+			}
+			else{
+				htmlString += `<li>${currentMonth[n].dayOfMonth}  <span style="font-size:0.7rem">£${currentMonth[n].night_price}</span></li>`;
+			}
+  		}
+		else{
+			htmlString += `<li><span class="active"> ${currentMonth[n].dayOfMonth}</span></li>`;
+		}
+	}
+	else{
+			htmlString += `<li><span class="active"> ${currentMonth[n].dayOfMonth}</span></li>`;
+	}
+  }
+					  
+  return htmlString;
+}
+
+
+// Next/previous month controls
+function plusMonth(n) {
+  monthIndex = checkMonthIndex(monthIndex += n, allDiary)
+  showMonth(monthIndex);
+}
+
+// Month view controls
+function selectAnyMonth(n) {
+  monthIndex = checkMonthIndex(monthIndex=n, allDiary)
+  showMonth(monthIndex);
+  return monthIndex;
+}
+
+//Check the slide number is in the correct range
+function checkMonthIndex(monthIndex, diaryData){
+  dateToday = new Date();
+  // Check the month index is in the array range
+  monthIndex = Math.min(monthIndex, diaryData.length-1);
+  monthIndex = Math.max(monthIndex, 0);
+  
+  // Check the month index is current, i.e. not in the past
+  for (let m = monthIndex; m < diaryData.length-2; m++){
+    currentMonth = diaryData[m];
+    if (dateToday>currentMonth[currentMonth.length-1].fullDate){
+	  monthIndex += 1;
+    }
+	else{break;}
+  }
+  
+  return monthIndex;
 }
