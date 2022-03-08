17a5b1dcafd4287e9b32f14e8be0343a5035a006
steffic17 <35765450+steffic17@users.noreply.github.com>
2022-03-08 14:34:26 +0000

    Image Format
    Update to image sizing dynamically to screen

--- slides-data.js
+++ slides-data.js
@@ -6,98 +6,98 @@ const getAllSlideData = () => {
 		slide_number: 1,
         total_slides: 14,
         slide_image: "Images/For Listing/SQ001 Chapel Outside - Front.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "The Chapel 1861"
 	};
 	slides[1] = {
 		slide_number: 2,
         total_slides: 14,
         slide_image: "Images/For Listing/SQ003 Chapel Outside - Patio - 1.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Outside dining area"
 	};
     slides[2] = {
 		slide_number: 3,
         total_slides: 14,
         slide_image: "Images/For Listing/SQ004 - Chapel Inside - Sitting - 3.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Open plan living area"
 	};
     slides[3] = {
 		slide_number: 4,
         total_slides: 14,
         slide_image: "Images/For Listing/SQ005 Chapel Inside - Kitchen - 3.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Open plan kitchen"
 	};
 	slides[4] = {
 		slide_number: 5,
         total_slides: 14,
         slide_image: "Images/For Listing/SQ006 Chapel Inside - Dining - 2.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Open plan dining area"
 	};
     slides[5] = {
 		slide_number: 6,
         total_slides: 14,
         slide_image: "Images/For Listing/SQ007 Chapel Inside - Landing - 4.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Galleried landing"
 	};
     slides[6] = {
 		slide_number: 7,
         total_slides: 14,
         slide_image: "Images/For Listing/008 Chapel Inside - Master 6.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Master suite"
 	};
     slides[7] = {
 		slide_number: 8,
         total_slides: 14,
         slide_image: "Images/For Listing/008a Chapel Inside - Master En-Suite 3.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "En-suite to master"
 	};
     slides[8] = {
 		slide_number: 9,
         total_slides: 14,
         slide_image: "Images/For Listing/009 Chapel Inside - Bed 2 - 2.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Bedroom 2"
 	};
     slides[9] = {
 		slide_number: 10,
         total_slides: 14,
         slide_image: "Images/For Listing/010 Chapel Inside - Bed 3 - 7.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Bedroom 3"
 	};
     slides[10] = {
 		slide_number: 11,
         total_slides: 14,
         slide_image: "Images/For Listing/011 - Chapel Inside - Bed 4 - 5.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Bedroom 4"
 	};
     slides[11] = {
 		slide_number: 12,
         total_slides: 14,
         slide_image: "Images/For Listing/012 - Chapel Inside - Bathroom - 1.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Family bathroom"
 	};
     slides[12] = {
 		slide_number: 13,
         total_slides: 14,
         slide_image: "Images/For Listing/SQ002 Chapel Outside - Front Door - 2.jpg",
-        style: "height:675px",
+        style: "height:90vh",
 		caption: "Welcome to The Chapel 1861"
 	};
     slides[13] = {
 		slide_number: 14,
         total_slides: 14,
         slide_image: "Images/Logo-tall.jpg",
-        style: "height:600px",
+        style: "height:90vh",
 		caption: "The Chapel 1861"
 	};
 	
